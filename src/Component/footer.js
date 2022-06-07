e733dd26756e6cf66f2009a523a65f67f9f436a4
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-07 20:03:13 +0500

    Create Footer.js

---
+++ src/Component/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+const Footer = () => (
+  <div className={styles.footer}>
+    <p>CopyRight @ 2022 Created by Fatima</p>
+  </div> 
+)
+
+export default Footer
